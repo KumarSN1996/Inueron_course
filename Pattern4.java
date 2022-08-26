2950ff476e98b9522b0276e18da01b535b3f8a06
Kumar Swamy N <104250670+KumarSN1996@users.noreply.github.com>
2022-08-27 00:50:01 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class Pattern4
+{
+	public static void main(String[] args) {
+	 {
+      Scanner s1 = new Scanner(System.in);
+       System.out.println("Enter the value of n");
+         int n = s1.nextInt();
+		
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 && i>=(n-1)/2||i==n-1||j==n-1&&i>=(n-1)/2&&i<=(n-1)||i-j>=(n-1)/2||i+j>=n-1+(n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+				
+			}
+			System.out.println();
+		}}}}
+		
